23b781750dc16f6cd863b3b09e951e0eeab39440
ocostalucas <olucascosta98@gmail.com>
2020-04-21 01:36:46 -0300

    added inputDecorationTheme

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:gif_search/screens/home.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
-    theme: ThemeData(hintColor: Colors.white),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+      )    
+    ),
   ));
 } 
 
